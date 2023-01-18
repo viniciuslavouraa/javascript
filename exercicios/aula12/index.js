52af9daa37da820c8bba7b612cd95e337e9eb7f6
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-01-17 21:29:25 -0300

    condições aninhadas e condição multipla

---
+++ exercicios/aula12/index.js
@@ -0,0 +1,13 @@
+var nome = 'vjoao'
+
+switch (nome) {
+    case 'pedro':
+        console.log('seu nome é: ' + nome)
+        break;
+    case 'vinicius' :
+        console.log(`Seu nome é ${nome}`)
+        break
+    default:
+        console.log('Nome inválido')
+        break;
+}
