1e9ed9ec836175a26abb7cc32b0bc8eafbcc16ac
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-10 17:02:38 -0300

    testei algumas coisas

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,8 +1,16 @@
-var c = 1
+var c = 0
+var num = 8
+
+do {
+    c++
+    console.log ( `Número ${c}`)
+} while ( c != num);
+console.log ( `Seu número é o ${c}`)
+/*var c = 1
 do {
     console.log (`passo ${c}`)
     c++
-} while (c <= 10);
+} while (c <= 10); */
 
 /*while (c <= 10) {
     console.log (`Passo ${c}`)
