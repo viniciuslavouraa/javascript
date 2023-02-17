12158f6d168bbc01b990bfe27da2c817f55b7711
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-17 14:24:22 -0300

    :+1:

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -3,10 +3,13 @@ btn.addEventListener ('click', gerar)
 
 function gerar () {
     var num = document.querySelector ('#numero')
-    var tabuada = 0
     var res = document.querySelector ('#res')
-    for (tabuada ; tabuada <=10 ; tabuada++){
+    for (tabuada = 0 ; tabuada <=10 ; tabuada++){
         var mult = num.value *tabuada
         res.innerHTML += `${num.value} x ${tabuada} = ${mult} <br>`
+    }
+
+    if (num.value == '') {
+        res.innerHTML = 'Digite um valor válido '
     }
 }
