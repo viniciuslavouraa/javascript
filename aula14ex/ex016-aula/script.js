f1ce77ad7365868cb4e15f35e7fa9e096da90f64
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-20 20:09:27 -0300

    contador feito em aula

--- aula14ex/ex016-aula/script.js
+++ aula14ex/ex016-aula/script.js
@@ -1,1 +1,34 @@
+var btn = document.querySelector ('#btn')
+btn.addEventListener ('click', contar)
 
+function contar () {
+    let tinicio = document.querySelector ('#inicio')
+    let tfim = document.querySelector ('#fim')
+    let tpasso = document.querySelector ('#passo')
+    let res = document.querySelector ('#res')
+
+    if (tinicio.value.length == 0 || tfim.value.length == 0 || tpasso.value.length == 0) {
+        res.innerHTML = '[ERRO] Faltam dados !!'
+    } else {
+        var inicio = Number(tinicio.value)
+        var fim = Number(tfim.value)
+        var passo = Number(tpasso.value)
+        res.innerHTML =  `Contando: <br>`
+
+        if (passo == 0) {
+            passo = 1
+        }
+
+        if (inicio <= fim) {
+            // contagem crescente
+            for (inicio ; inicio <= fim ; inicio += passo) {
+            res.innerHTML += ` ${inicio} \u{1F449} `
+            } 
+        } else {
+            // contagem decrescente
+            for (inicio ; inicio >= fim ; inicio -= passo) {
+            res.innerHTML += ` ${inicio} \u{1F449} `
+        }
+        }
+    }
+}
