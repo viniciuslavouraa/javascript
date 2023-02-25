63faf8a293478ee258a0738eb7b8d1f99070889b
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-24 21:48:58 -0300

    estudo de arrays

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,15 @@
+
+var num = [5, 8, 2, 9,3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let posiçao = num.indexOf(5)
+if(posiçao == -1) {
+    console.log('o valor não foi encontrado')
+}else {
+    console.log(`o valor 8 está na posição ${posiçao}`)
+}
+
---
+++ exercicios/aula15/vetornatela.js
@@ -0,0 +1,11 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+console.log(valores)
+for(let pos in valores) {
+    console.log(` a posição ${pos} tem os valores ${valores[pos]}`)
+}
+
+
+/*for(pos ; pos <= valores.length ; pos++) {
+    console.log(`a posição ${pos} tem os valores ${valores[pos]} `)
+} */
