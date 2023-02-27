748429a683862cc962f55c6a14f2312dd668a4f9
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-27 20:24:35 -0300

    aula sobre funções

---
+++ exercicios/aula16/funcao02.js
@@ -0,0 +1,4 @@
+function soma(n1= 0, n2= 0) { //posso pré-definir valores dos paremetros
+    return n1 + n2
+}
+console.log(soma(2))
---
+++ exercicios/aula16/funcao03.js
@@ -0,0 +1,4 @@
+let v  = function(x) { // da pra uma variavel receber uma função
+    return x * 2
+}
+console.log(v(5))
